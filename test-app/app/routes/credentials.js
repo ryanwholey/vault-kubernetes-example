ee378d2f78a2d98321473c339c8d41440d8d40be
Ryan Wholey <ryan@takescoop.com>
2020-02-13 20:05:43 -0800

    Return direct knex connection creds

--- test-app/app/routes/credentials.js
+++ test-app/app/routes/credentials.js
@@ -2,6 +2,7 @@
 
 const knex = require('../lib/knex')
 const config = require('../config')
+const _ = require('lodash')
 
 module.exports = {
   method: 'GET',
@@ -9,25 +10,22 @@ module.exports = {
   config: {},
   handler: async function(request) {
     let version
-    let status
+
     try {
       version = await knex.select(knex.raw('VERSION()'))
-      status = version ? 'OK' : 'NOT OK'
     } catch (err) {
-      status = 'NOT OK'
+    
     }
-
+    const status = version ? 'OK' : 'FAILURE'
     console.log(`${new Date().toISOString()}: ${request.url}: ${status}`)
     const { username, password } = new URL(config.pgConnectionString)
     console.log(`\t user: ${username}`)
     console.log(`\t pass: ${password}`)
     console.log(`\t pod: ${process.env.POD_NAME}`)
-
+  
     return {
-      pgVersion: version && version[0].version,
-      status, 
-      username,
-      password,
+      ..._.pick(knex.client.config.connection, ['user', 'password']),
+      status,
       podName: config.podName
     }
   }
